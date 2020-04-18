93b224f60cac4de96097efe9a9b8619a81adfd48
Stephan Mühl <31169771+Blueforcer@users.noreply.github.com>
2020-04-18 12:10:49 +0200

    Update fimwareUpdater.bat

--- fimwareUpdater.bat
+++ fimwareUpdater.bat
@@ -9,26 +9,17 @@ set White=%ESC%[37m
 title Blueforcers Quest Updater
 ECHO %Green%Blueforcers Quest Updater
 ECHO.
-ECHO %Green%---Connect your Quest to the PC and start by choosing your desired Firmware---
+:checkfile
+set /p DUMMY= %Green%---Connect your Quest to the PC and press enter---
 ECHO.
-ECHO %White%1. v14
-ECHO %White%2. v15
-set choice=
-set /p choice=Select Firmware: 
-if not '%choice%'=='' set choice=%choice:~0,1%
-if '%choice%'=='1' goto 14
-if '%choice%'=='2' goto 15
-ECHO "%choice%" is not valid, try again
-ECHO.
-goto start
-:14
-set fw=v14.zip
-ECHO %Green%Installing Oculus Quest Firmware v14. Please Wait...
-goto prepare
-:15
-set fw=v15.zip
-ECHO %Green%Installing Oculus Quest Firmware v15. Please Wait...
-goto prepare
+
+
+if exist update.zip (
+    GOTO prepare
+) else (
+    echo %Red%update.zip missing!
+GOTO checkfile
+)
 :prepare
 ECHO %Green%List ADB Devices ...
 ECHO %White%
